fb02fa8356a524b6dca62e70d4a2ce97406672b3
jaroslav.h <jaroslav.hin@ukr.net>
2018-05-08 14:13:01 +0300

    Buss1, Buss2

--- app/src/main/java/hin/jaroslav/bus/Buss2.java
+++ app/src/main/java/hin/jaroslav/bus/Buss2.java
@@ -1,24 +1,16 @@
 package hin.jaroslav.bus;
 
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class Buss1 extends AppCompatActivity {
+public class Buss2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_buss1);
+        setContentView(R.layout.activity_buss2);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
+        
     }
-
-    public void rozkl_zag(View view) {
-        Intent intent = new Intent(this, Buss2.class);
-        startActivity(intent);
-    }
-
 }
