2d3edb51d5be67aad578e729779d2d511b1e8f08
jaroslav.hin <jaroslav.hin@ukr.net>
2019-01-01 20:54:38 +0200

    gridview по центру

--- app/src/main/java/hin/jaroslav/bus/Buss2.java
+++ app/src/main/java/hin/jaroslav/bus/Buss2.java
@@ -1,8 +1,17 @@
 package hin.jaroslav.bus;
 
+import android.app.Activity;
 import android.content.pm.ActivityInfo;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class Buss2 extends AppCompatActivity {
 
@@ -11,6 +20,5 @@ public class Buss2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buss2);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        
-    }
+        }
 }
